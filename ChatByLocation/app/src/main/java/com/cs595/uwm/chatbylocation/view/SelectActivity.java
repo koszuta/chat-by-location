ecd23436247184e9d86d2679b18769c453179f90
STORMY\gnovak.sh <gsnovak@uwm.edu>
2017-04-11 22:17:09 -0500

    Demo for new room join that I created (Geofencing in beginning stages).

--- ChatByLocation/app/src/main/java/com/cs595/uwm/chatbylocation/view/SelectActivity.java
+++ ChatByLocation/app/src/main/java/com/cs595/uwm/chatbylocation/view/SelectActivity.java
@@ -1,118 +1,292 @@
 package com.cs595.uwm.chatbylocation.view;
 
+import android.Manifest;
 import android.app.DialogFragment;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ListView;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.cs595.uwm.chatbylocation.objModel.RoomIdentity;
 import com.cs595.uwm.chatbylocation.R;
 import com.cs595.uwm.chatbylocation.service.Database;
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.database.FirebaseListAdapter;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.FirebaseDatabase;
+import com.cs595.uwm.chatbylocation.service.GeofenceTransitionService;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-/**
- * Created by Nathan on 3/13/17.
- */
+import java.util.ArrayList;
+import java.util.List;
 
-public class SelectActivity extends AppCompatActivity {
+public class SelectActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, GoogleMap.OnMapClickListener, GoogleMap.OnMarkerClickListener, ResultCallback<Status> {
+    private static final String TAG = SelectActivity.class.getSimpleName();
+    private static final float GEOFENCE_RADIUS = 1000;
+    private static final String GEOFENCE_REQ_ID = "Geofence";
+    private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private static final long GEO_DURATION = 60 * 60 * 1000;
+    TextView mLatitudeText;
+    TextView mLongitudeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_layout);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
-//        if(Database.getUserCurrentRoomID() != null){
-//            Intent intent = new Intent(SelectActivity.getContext(), ChatActivity.class);
-//            startActivity(intent);
-//        }
+        createGoogleApi();
 
-        displayRoomList();
+        Context context = getApplicationContext();
+        CharSequence text = "Click to join or create room!";
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
-    public void joinRoomClick(View view) {
-        Database.setUserRoom(String.valueOf(view.getTag()));
-        startActivity(new Intent(getApplicationContext(), ChatActivity.class));
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
     }
 
-    public void createRoomClick(View view) {
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        Log.d(TAG, "onMapClick("+latLng +")");
         DialogFragment dialog = new CreateRoomDialog();
         dialog.show(getFragmentManager(), "create room");
-    }
-
-    private void displayRoomList() {
-        ListView listOfRooms = (ListView) findViewById(R.id.roomList);
-
-        final FirebaseListAdapter<RoomIdentity> adapter = new FirebaseListAdapter<RoomIdentity>(this,
-                RoomIdentity.class,
-                R.layout.room_list_item,
-                FirebaseDatabase.getInstance().getReference().child("roomIdentity")) {
-            @Override
-            protected void populateView(View view, RoomIdentity roomIdentity, int position) {
-                TextView roomName = (TextView) view.findViewById(R.id.roomName);
-                TextView roomCoords = (TextView) view.findViewById(R.id.roomCoords);
-                TextView roomRadius = (TextView) view.findViewById(R.id.roomRadius);
-
-                roomName.setText(roomIdentity.getName());
-                roomCoords.setText(roomIdentity.getLongg() + ", " + roomIdentity.getLat());
-                roomRadius.setText("Radius: " + roomIdentity.getRad() + "m");
-
-                Button joinButton = (Button) view.findViewById(R.id.joinButton);
-                joinButton.setTag(getRef(position).getKey());
-
-            }
-        };
-
-        listOfRooms.setAdapter(adapter);
+        createMarkerForGeofence(latLng);
+        drawGeofence();
+        markerLocation(latLng);
+        startGeofence();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.select_view_menu, menu);
+    public boolean onMarkerClick(final Marker marker) {
+        Double lat = marker.getPosition().latitude;
+        Double lon = marker.getPosition().longitude;
+        //Todo Ask Lowell
+        //Database.setUserRoom(String.valueOf(view.getTag()));
+        startActivity(new Intent(getApplicationContext(), ChatActivity.class));
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu_sign_out:
-                AuthUI.getInstance()
-                        .signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        Toast.makeText(SelectActivity.this,
-                                "You have been signed out.",
-                                Toast.LENGTH_LONG)
-                                .show();
+    private Marker marker;
+    private void createMarkerForGeofence(LatLng latLng) {
+        Log.i(TAG, "markerForGeofence("+latLng+")");
+        String title = latLng.latitude + ", " + latLng.longitude;
+        // Define marker options
+        MarkerOptions markerOptions = new MarkerOptions()
+                .position(latLng)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                .title(title);
+            marker = mMap.addMarker(markerOptions);
+    }
 
-                        // Close activity
-                        finish();
-                    }
-                });
-                break;
-            case R.id.menu_settings:
-                Intent settingsIntent = new Intent(this, SettingsActivity.class);
-                startActivity(settingsIntent);
-                break;
-            case R.id.menu_bypass:
-                Intent intent = new Intent(this, ChatActivity.class);
-                startActivity(intent);
-                break;
-            default:
-                break;
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
         }
-        return true;
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+        }
     }
+
+    // Create GoogleApiClient instance
+    private void createGoogleApi() {
+        if ( mGoogleApiClient == null ) {
+            mGoogleApiClient = new GoogleApiClient.Builder( this )
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener( this )
+                    .addApi( LocationServices.API )
+                    .build();
+        }
+    }
+
+    private static final String NOTIFICATION_MSG = "NOTIFICATION MSG";
+    // Create a Intent send by the notification
+    public static Intent makeNotificationIntent(Context context, String msg) {
+        Intent intent = new Intent( context, MainActivity.class );
+        intent.putExtra( NOTIFICATION_MSG, msg );
+        return intent;
+    }
+
+    private void startGeofence() {
+        Log.i(TAG, "startGeofence()");
+        if( marker != null ) {
+            Geofence geofence = createGeofence( marker.getPosition(), GEOFENCE_RADIUS );
+            GeofencingRequest geofenceRequest = createGeofenceRequest( geofence );
+            addGeofence( geofenceRequest );
+        } else {
+            Log.e(TAG, "Geofence marker is null");
+        }
+    }
+
+    // Create a Geofence Request
+    private GeofencingRequest createGeofenceRequest( Geofence geofence ) {
+        Log.d(TAG, "createGeofenceRequest");
+        return new GeofencingRequest.Builder()
+                .setInitialTrigger( GeofencingRequest.INITIAL_TRIGGER_ENTER )
+                .addGeofence( geofence )
+                .build();
+    }
+
+    // Create a Geofence
+    private Geofence createGeofence( LatLng latLng, float radius ) {
+        Log.d(TAG, "createGeofence");
+        return new Geofence.Builder()
+                .setRequestId(GEOFENCE_REQ_ID)
+                .setCircularRegion( latLng.latitude, latLng.longitude, radius)
+                .setExpirationDuration( GEO_DURATION )
+                .setTransitionTypes( Geofence.GEOFENCE_TRANSITION_ENTER
+                        | Geofence.GEOFENCE_TRANSITION_EXIT )
+                .build();
+    }
+
+    private Circle geoFenceLimits;
+    private void drawGeofence() {
+        Log.d(TAG, "drawGeofence()");
+
+        if ( geoFenceLimits != null )
+            geoFenceLimits.remove();
+
+        CircleOptions circleOptions = new CircleOptions()
+                .center( marker.getPosition())
+                .strokeColor(Color.argb(50, 70,70,70))
+                .fillColor( Color.argb(100, 150,150,150) )
+                .radius( GEOFENCE_RADIUS );
+        geoFenceLimits = mMap.addCircle( circleOptions );
+    }
+
+    // Add the created GeofenceRequest to the device's monitoring list
+    private void addGeofence(GeofencingRequest request) {
+        Log.d(TAG, "addGeofence");
+        if (checkPermission())
+            LocationServices.GeofencingApi.addGeofences(
+                    mGoogleApiClient,
+                    request,
+                    createGeofencePendingIntent()
+            ).setResultCallback(this);
+    }
+
+    // Saving GeoFence marker with prefs mng
+    private void saveGeofence() {
+        //Todo save geofence as room
+    }
+
+    // Check for permission to access Location
+    private boolean checkPermission() {
+        Log.d(TAG, "checkPermission()");
+        // Ask for permission if it wasn't granted yet
+        return (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED );
+    }
+
+    private PendingIntent geoFencePendingIntent;
+    private final int GEOFENCE_REQ_CODE = 0;
+    private PendingIntent createGeofencePendingIntent() {
+        Log.d(TAG, "createGeofencePendingIntent");
+        if ( geoFencePendingIntent != null )
+            return geoFencePendingIntent;
+
+        Intent intent = new Intent( this, GeofenceTransitionService.class);
+        return PendingIntent.getService(
+                this, GEOFENCE_REQ_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT );
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Location defaultLocation = new Location("");
+        defaultLocation.setLongitude(33.86);
+        defaultLocation.setAltitude(151.20);
+        ArrayList<Location> rooms = new ArrayList<>();
+        rooms.add(defaultLocation);
+
+        mMap = googleMap;
+        mMap.setOnMapClickListener(this);
+        mMap.setOnMarkerClickListener(this);
+
+        // Todo add each room from Firebase
+        for(Location room: rooms)
+        {
+            LatLng roomCoordinate = new LatLng(room.getLongitude(), room.getLatitude());
+            mMap.addMarker(new MarkerOptions().position(roomCoordinate).title("Room"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(roomCoordinate));
+        }
+    }
+
+    private Marker locationMarker;
+    private void markerLocation(LatLng latLng) {
+        Log.i(TAG, "markerLocation("+latLng+")");
+        String title = latLng.latitude + ", " + latLng.longitude;
+        MarkerOptions markerOptions = new MarkerOptions()
+                .position(latLng)
+                .title(title);
+        if ( mMap!=null ) {
+            if ( locationMarker != null )
+                locationMarker.remove();
+            locationMarker = mMap.addMarker(markerOptions);
+            float zoom = 14f;
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, zoom);
+            mMap.animateCamera(cameraUpdate);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onResult(@NonNull Status status) {
+            Log.i(TAG, "onResult: " + status);
+            if ( status.isSuccess() ) {
+                saveGeofence();
+                drawGeofence();
+            } else {
+                //Todo pretty failure message
+            }
+        }
 }
